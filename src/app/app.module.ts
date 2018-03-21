f02c246353a9402816f5d0f9fa326ec2f9372705
Nicki Kim Feldt <nick135f@edu.easj.dk>
2018-03-21 10:38:21 +0100

    Updated with project copypast thingy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieService } from './movie.service';
+//import { MoviePosterComponent } from './movie-poster/movie-poster.component';
+//import { MoviePlotComponent } from './movie-plot/movie-plot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+ //   MoviePosterComponent,
+ //   MoviePlotComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [MovieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
